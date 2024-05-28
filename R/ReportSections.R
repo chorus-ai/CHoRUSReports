a4307b8eab8d622576d40ffb4f8e22c2169354ff
jshoughtaling <jared.houghtaling@tuftsmedicalcenter.org>
2024-05-28 19:28:21 -0400

    Add working report sections

--- R/ReportSections.R
+++ R/ReportSections.R
@@ -13,6 +13,7 @@
 #' \code{ReportSections} the proposed structure can be found here: https://github.com/chorus-ai/chorus-cloud-pm/issues/3#issuecomment-2069945869
 #'
 #' @param connectionDetails                An R object of type \code{connectionDetails} created using the function \code{createConnectionDetails} in the \code{DatabaseConnector} package.
+#' @param connectionDetailsMerge           An R object of type \code{connectionDetails} created using the function \code{createConnectionDetails} in the \code{DatabaseConnector} package.
 #' @param cdmDatabaseSchema    	           Fully qualified name of database schema that contains OMOP CDM schema.
 #'                                         On SQL Server, this should specifiy both the database and the schema, so for example, on SQL Server, 'cdm_instance.dbo'.
 #' @param resultsDatabaseSchema		         Fully qualified name of database schema that we can write final results to. Default is cdmDatabaseSchema.
@@ -34,40 +35,156 @@
 #' @return                                 An object of type \code{achillesResults} containing details for connecting to the database containing the results
 #' @export
 
-createReportSections <- function (connectionDetails,
-                                 cdmDatabaseSchema,
-                                 databaseName = "",
-                                 databaseId = "",
-                                 databaseDescription = "",
-                                 smallCellCount = 5,
-                                 runVocabularyChecks = TRUE,
-                                 runDataTablesChecks = TRUE,
-                                 sqlOnly = FALSE,
-                                 outputFolder = "output",
-                                 verboseMode = TRUE,
-                                 accountUrl = "",
-                                 accountKey = "",
-                                 accountName = "") {
-    reportSections <- hash()
-
-    # SECTION 1
-
-    section1 <- hash()
-    metadata <- hash()
-    overview <- hash()
-    patientCounts <- hash()
-    differential <- hash()
-    allFiles <- get_blob_files(accountUrl, accountKey, accountName)
-    allFiles <- allFiles %>% janitor::clean_names() # eliminate hash in names from Azure
-    metadata[['deliveryTime']] <- max(allFiles$last_modified) # get delivery time
-    metadata[['packetSize']] <- round(sum(allFiles$size)/1000000000, digits=2) # get delivery packet size in gb
-    metadata[['numOfPriorDeliveries']] <- 0 # TODO get_prior_deliveries(site)
-    metadata[['recentFeedback']] <- c() # TODO get_latest_feedback(site)
-    section1[['Metadata']] <- metadata
-
-    dqd_overview <- querySql(conn,"select category,  sum(passed) as passed, sum(failed) as failed FROM omopcdm.dqdashboard_results GROUP BY category ORDER BY 3 desc;")
-    overview[['filesDelivered']] <- nrow(allFiles)
-    overview[['qualityChecks']] <- dqd_overview
-    section1[['Overview']] <- overview
-
-    reportSections[['section1']] <- section1
+createReportSections <- function  (connectionDetails,
+                                   connectionDetailsMerge,
+                                   cdmDatabaseSchema,
+                                   databaseName = "",
+                                   databaseId = "",
+                                   smallCellCount = 5,
+                                   sqlOnly = FALSE,
+                                   outputFolder = "output",
+                                   verboseMode = TRUE,
+                                   accountUrl = "",
+                                   accountKey = "",
+                                   containerName = "",
+                                   depth = 3) {
+  reportSections <- hash()
+  TARGET_PAT_CNT <- 8000
+  conn <- connect(connectionDetails)
+  connMerge <- connect(connectionDetailsMerge)
+  
+  # SECTION 1 - Metadata & Overviews
+  
+  section1 <- hash()
+  metadata <- hash()
+  overview <- hash()
+  patientCounts <- hash()
+  differential <- hash()
+  allFiles <- list_blobs(accountUrl, accountKey, containerName)
+  allFiles <- allFiles %>% janitor::clean_names() # eliminate hash in names from Azure
+  dbWriteTable(conn, "public.allFiles", allFiles, overwrite = TRUE)
+  metadata[['deliveryTime']] <- max(allFiles$last_modified) # get delivery time
+  metadata[['packetSize']] <- querySql(conn,"select sum(size)/1000000000 FROM public.allfiles;")[[1]] # get delivery packet size in gb
+  metadata[['numOfPriorDeliveries']] <- 0 # TODO get_prior_deliveries(databaseName)
+  metadata[['recentFeedback']] <- c("NO FEEDBACK YET") # TODO get_latest_feedback(databaseName)
+  section1[['Metadata']] <- metadata
+  
+  dqdOverview <- querySql(conn,"select  
+                            round(sum(passed)/(sum(passed) + sum(failed))::numeric, 2) as pcnt
+                            FROM omopcdm.dqdashboard_results ;")
+  overview[['filesDelivered']] <- nrow(allFiles)
+  overview[['qualityChecks']] <- dqdOverview
+  overview[['phiIssuesOMOP']] <- querySql(conn,"select count(*) from omopcdm.phi_output WHERE pred_result = '1';")[[1]]
+  overview[['phiIssuesWAVE']] <- "TBD"
+  overview[['phiIssuesIMAG']] <- "TBD"
+  overview[['phiIssuesNOTE']] <- "TBD"
+  overview[['chorusQC']] <- 'TBD' # TODO get_chorus_qc(databaseName)
+  overview[['chorusChars']] <- 'TBD'
+  section1[['Overview']] <- overview
+  
+  
+  sqlAllfiles <- glue::glue("
+    ALTER TABLE public.allfiles ADD COLUMN person text;
+    ALTER TABLE public.allfiles ADD COLUMN modality text;
+    ALTER TABLE public.allfiles ADD COLUMN filename text;
+    ALTER TABLE public.allfiles ADD COLUMN extension text;
+    
+    UPDATE public.allfiles
+    SET person = NULLIF(split_part(name, '/', {depth}-2), ''),
+        modality = NULLIF(split_part(name, '/', {depth}-1), ''),
+        filename = NULLIF(split_part(name, '/', {depth}), '')
+    WHERE name IS NOT NULL;
+    
+    UPDATE public.allfiles
+    SET extension = split_part(filename, '.', -1)
+    WHERE filename IS NOT NULL;
+  ")
+  
+  executeSql(conn, sqlAllfiles, progressBar = FALSE, reportOverallTime = FALSE)
+  waveCounts <- querySql(conn,glue::glue("select count(DISTINCT person) from public.allfiles WHERE lower(modality) LIKE 'wave%' AND extension IS NOT NULL;"))[[1]]
+  waveFileCounts <- querySql(conn,glue::glue("select count(*) from public.allfiles WHERE lower(modality) LIKE 'wave%' AND extension IS NOT NULL;"))[[1]]
+  imgCounts <- querySql(conn,"select count(DISTINCT person) from public.allfiles WHERE lower(modality) LIKE 'imag%' AND extension IS NOT NULL;")[[1]]
+  imgFileCounts <- querySql(conn,"select count(*) from public.allfiles WHERE lower(modality) LIKE 'imag%' AND extension IS NOT NULL;")[[1]]
+  omopCounts <- querySql(conn,"select count(DISTINCT person) from public.allfiles WHERE lower(modality) LIKE 'omop%' AND extension IS NOT NULL;")[[1]]
+  omopFileCounts <- querySql(conn,"select count(*) from public.allfiles WHERE lower(modality) LIKE 'omop%' OR lower(person) = 'omop' AND extension IS NOT NULL;")[[1]]
+  if (identical(omopCounts,integer(0))) {
+    omopCounts <- querySql(conn,"SELECT COUNT(DISTINCT person_id) FROM omopcdm.person")[[1]]
+  }
+  noteCounts <- querySql(conn,"SELECT COUNT(DISTINCT person_id) FROM omopcdm.note")[[1]]
+  personsAnyModes <- max(waveCounts, imgCounts, omopCounts) #TODO Add noteCounts eventually
+  personsAllModes <- min(waveCounts, imgCounts, omopCounts) #TODO Add noteCounts eventually
+  
+  
+  patientCounts[['anyDataPersons']] <- personsAnyModes
+  patientCounts[['anyDataPersonsPct']] <- personsAnyModes / TARGET_PAT_CNT
+  patientCounts[['anyDataPersonsDelta']] <- 0 # get_patient_counts_delta('any')
+  patientCounts[['allDataPersons']] <- personsAllModes
+  patientCounts[['allDataPersonsPct']] <- personsAllModes / TARGET_PAT_CNT
+  patientCounts[['allDataPersonsDelta']] <- 0 # get_patient_counts_delta('all')
+  patientCounts[['omopDataFiles']] <- omopFileCounts
+  patientCounts[['omopDataPersons']] <- omopCounts
+  patientCounts[['omopDataPersonsPct']] <- omopCounts / TARGET_PAT_CNT
+  patientCounts[['omopDataPersonsDelta']] <- 0 # get_patient_counts_delta('omop')
+  patientCounts[['imageDataFiles']] <- imgFileCounts
+  patientCounts[['imageDataPersons']] <- imgCounts
+  patientCounts[['imageDataPersonsPct']] <- imgCounts / TARGET_PAT_CNT
+  patientCounts[['imageDataPersonsDelta']] <- 0 # get_patient_counts_delta('image')
+  patientCounts[['waveDataFiles']] <- waveFileCounts
+  patientCounts[['waveDataPersons']] <- waveCounts
+  patientCounts[['waveDataPersonsPct']] <- waveCounts / TARGET_PAT_CNT
+  patientCounts[['waveDataPersonsDelta']] <- 0 # get_patient_counts_delta('wave')
+  patientCounts[['noteDataPersons']] <- noteCounts
+  patientCounts[['noteDataPersonsPct']] <- noteCounts / TARGET_PAT_CNT
+  patientCounts[['noteDataPersonsDelta']] <- 0 # get_patient_counts_delta('note')
+  approvedPersons <- 0 # TODO get_approved_persons()
+  patientCounts[['allDataApprovedPersons']] <- approvedPersons
+  patientCounts[['allDataApprovedPersonsPct']] <- approvedPersons / TARGET_PAT_CNT
+  patientCounts[['allDataApprovedPersonsDelta']] <- 0 # get_patient_counts_delta('approved')
+  deidPersons <- 0 # TODO get_deid_persons()
+  patientCounts[['allDataApprovedDeidPersons']] <- deidPersons
+  patientCounts[['allDataApprovedDeidPersonsPct']] <- personsAllModes / TARGET_PAT_CNT
+  patientCounts[['allDataApprovedDeidPersonsDelta']] <- 0 # get_patient_counts_delta('deid')
+  
+  section1[['PatientCounts']] <- patientCounts
+  
+  reportSections[['section1']] <- section1
+  
+  # SECTION 2 - PHI
+  
+  section2 <- hash()
+  phiOverview <- hash()
+  
+  phiOverview[['omop']] <- querySql(conn,"select * from omopcdm.phi_output order by phi_prob DESC, tab ASC, col ASC;")
+  phiOverview[['wave']] <- 0 # get_wave_phi()
+  phiOverview[['imag']] <- 0 # get_imag_phi()
+  phiOverview[['note']] <- 0 # get_note_phi()
+  
+  section2[['phiOverview']] <- phiOverview
+  
+  reportSections[['section2']] <- section2
+  
+  # SECTION 3 - Data Quality
+  
+  section3 <- hash()
+  dqdResults <- hash()
+  
+  dqdResults[['qualityPerCheck']] <- querySql(conn, "select cdm_table_name, category, count(*) AS count_failed FROM omopcdm.dqdashboard_results WHERE failed = '1' GROUP BY cdm_table_name, category ORDER BY 3 DESC;")
+  
+  section3[['DQDResults']] <- dqdResults
+  
+  reportSections[['section3']] <- section3
+  
+  # SECTION 4 - Cohorts and Characterization
+  
+  section4 <- hash()
+  cohortsCounts <- hash()
+  
+  cohortsCounts[['topInNetwork']] <- querySql(connMerge,glue::glue("select description, cnt_merge, percent_merge, cnt_{databaseName} from omopcdm.cohort_reports ORDER BY cnt_merge DESC limit 20;"))
+  cohortsCounts[['topInSource']] <- querySql(connMerge,glue::glue("select description, cnt_merge, percent_merge, cnt_{databaseName} from omopcdm.cohort_reports ORDER BY cnt_{databaseName} DESC limit 20;"))
+  
+  section4[['CohortCounts']] <- cohortsCounts
+  reportSections[['section4']] <- section4
+  
+  return(reportSections)
+}
+    
