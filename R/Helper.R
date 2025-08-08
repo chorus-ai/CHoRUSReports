963b86b283de0bbaaabfb58dda39b80cb38bc1d1
jshoughtaling <jared.houghtaling@tuftsmedicalcenter.org>
2025-08-08 16:09:39 +0200

    Fix large object handling

--- R/Helper.R
+++ R/Helper.R
@@ -20,7 +20,7 @@
 getPriorReleaseRds <- function(connectionDetails, priorOid, databaseName) {
   pgHost <- strsplit(connectionDetails$server(), "/")[[1]][1]
   rdsFile <- glue::glue("/tmp/prior_{databaseName}.rds")
-  retrieveLO <- glue::glue("export PGPASSWORD=\"{connectionDetails$password()}\" && psql -h {pgHost} -U postgres -At -c \"\\lo_export {priorOid} '{rdsFile}'\"")
+  retrieveLO <- glue::glue("export PGPASSWORD=\"{connectionDetails$password()}\" && psql -h {pgHost} -d {databaseName} -U postgres -At -c \"\\lo_export {priorOid} '{rdsFile}'\"")
   success <- system(retrieveLO, intern=TRUE)
   prior_results <- readRDS(rdsFile)
   return(prior_results)
